049a288325dd209205b5a0b2d96c98f3bedf8434
Tanmaya <tanmayanaik2006@gmail.com>
2025-06-20 19:41:33 +0530

    basic structure made

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", title="Home")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About", desciption="This is the about us page!")
+
+@app.route("/contact")
+def contact():
+    pass
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html", title="Projects", description="Project page, build in progress...")
+
+if __name__ == "__main__":
+    app.run(debug=True)
