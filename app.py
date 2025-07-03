7de2338c335b0e248280165bb54304c5a000b7a9
Tanmaya Kumar Naik <150720663+Tanmaya793@users.noreply.github.com>
2025-07-03 09:44:25 +0530

    Database connected

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import Contact
-from datas import messages, add_message
+# from datas import messages, add_message
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select, engine
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "5d50c38d0f12ca64cddd64b56c7baeab"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:tana9861751892%40@localhost:3306/portfolio"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Messages(db.Model):
+    firstname = db.Column(db.String(20))
+    lastname = db.Column(db.String(20))
+    email = db.Column(db.String(50), primary_key = True)
+    message = db.Column(db.Text)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 @app.route("/home")
@@ -18,7 +32,13 @@ def about():
 def contact():
     form = Contact()
     if form.validate_on_submit():
-        add_message(form.firstname.data, form.lastname.data, form.email.data, form.message.data)
+        firstname = request.form["firstname"]
+        lastname = request.form["lastname"]
+        email = request.form["email"]
+        message = request.form["message"]
+        new_msg = Messages(firstname=firstname, lastname=lastname, email=email, message=message)
+        db.session.add(new_msg)
+        db.session.commit()
         flash("Your message has been successfully sent, Thank you!")
         return redirect(url_for("contact")) # PRG pattern (Post-Redirect-Get)
     return render_template("contact.html", title="Contact Me", form=form)
@@ -26,9 +46,11 @@ def contact():
 
 @app.route("/admin/<pas>")
 def admin(pas):
-    if pas=="mysecretpass":
-        return render_template("admin.html", messages=messages)
-    return redirect(url_for("home"))
+    if pas == "mysecretpass":
+        messages = Messages.query.all() 
+        print(messages)
+        return render_template("admin.html", messages=messages) 
+    return redirect(url_for("home")) 
 
 @app.route("/projects")
 def projects():
